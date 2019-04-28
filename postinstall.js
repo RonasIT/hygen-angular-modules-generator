85fa6161942f376293961bea7734dfa8be56e4d0
astorozhevsky <astorozhevsky@ronasit.com>
2019-04-28 21:10:20 +0600

    updated postinstall script

--- postinstall.js
+++ postinstall.js
@@ -1,20 +1,31 @@
-const FSP = require('fs').promises;
-const Path = require('path');
+// const FSP = require('fs').promises;
+// const Path = require('path');
 
-async function copyDir(src, dest) {
-  const entries = await FSP.readdir(src, { withFileTypes: true });
-  await FSP.mkdir(dest);
-  for (let entry of entries) {
-    const srcPath = Path.join(src, entry.name);
-    const destPath = Path.join(dest, entry.name);
-    if (entry.isDirectory()) {
-      await copyDir(srcPath, destPath);
-    } else {
-      await FSP.copyFile(srcPath, destPath);
-    }
+// async function copyDir(src, dest) {
+//   const entries = await FSP.readdir(src, { withFileTypes: true });
+//   await FSP.mkdir(dest);
+//   for (let entry of entries) {
+//     const srcPath = Path.join(src, entry.name);
+//     const destPath = Path.join(dest, entry.name);
+//     if (entry.isDirectory()) {
+//       await copyDir(srcPath, destPath);
+//     } else {
+//       await FSP.copyFile(srcPath, destPath);
+//     }
+//   }
+// }
+
+// console.log(process.env.INIT_CWD);
+
+// copyDir('_templates/', process.env.INIT_CWD + '/_templates/');
+
+var ncp = require('ncp').ncp;
+var fs = require('fs');
+
+ncp('_templates/', process.env.INIT_CWD + '/_templates/', function (error) {
+  if (error) {
+    return console.error(error);
   }
-}
 
-console.log(process.env.INIT_CWD);
-
-copyDir('_templates/', process.env.INIT_CWD + '/_templates/');
+  fs.appendFileSync(rocess.env.INIT_CWD + '/.gitignore', '_templates/');
+});
