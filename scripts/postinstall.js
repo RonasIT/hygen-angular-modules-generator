37ebf8484a747286eed73c54302ac168c06d7148
astorozhevsky <astorozhevsky@ronasit.com>
2019-04-28 20:21:48 +0600

    added postinstall script

---
+++ scripts/postinstall.js
@@ -0,0 +1,17 @@
+const FSP = require('fs').promises;
+
+async function copyDir(src, dest) {
+  const entries = await FSP.readdir(src, { withFileTypes: true });
+  await FSP.mkdir(dest);
+  for (let entry of entries) {
+    const srcPath = Path.join(src, entry.name);
+    const destPath = Path.join(dest, entry.name);
+    if (entry.isDirectory()) {
+      await copyDir(srcPath, destPath);
+    } else {
+      await FSP.copyFile(srcPath, destPath);
+    }
+  }
+}
+
+await copyDir('../templates', '../../../../templates');
